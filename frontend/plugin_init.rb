6319b60c3a39cc4ad287a48635839f4a8dd13618
Kevin Clair <kevin@jackflaps.net>
2024-05-10 08:40:14 -0400

    add a frontend announcement to the front page

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,2 +1,3 @@
 AppConfig[:frontend_branding_img] = 'assets/pennstate.png'
 AppConfig[:frontend_branding_img_alt_text] = 'Penn State University Libraries ArchivesSpace Staff Interface'
+AppConfig[:display_frontend_announcement] = true
